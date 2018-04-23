0eecad0a988728ae20ec0b3d84adae23b13358f3
SotirisKot <sotiriskot9@gmail.com>
2018-04-23 23:47:14 +0300

    Edits

---
+++ app/src/main/java/gr/aueb/softeng/project1801/BusState.java
@@ -0,0 +1,10 @@
+package gr.aueb.softeng.project1801;
+
+public enum BusState {
+
+    //The bus is available for a new route
+    AVAILABLE,
+
+    //The bus is not available for a new route
+    NOT_AVAILABLE
+}
---
+++ app/src/main/java/gr/aueb/softeng/project1801/DriverState.java
@@ -0,0 +1,11 @@
+package gr.aueb.softeng.project1801;
+
+public enum DriverState {
+
+    //The driver is available for a new route
+    AVAILABLE,
+
+    //The driver is not available for a new route
+    NOT_AVAILABLE
+
+}
