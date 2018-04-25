3a78d6cb414d3414fb3977dde5c64e2fe9aa6f91
GeorgeChatzo <giorgoschatzopoulos7@gmail.com>
2018-04-25 17:45:37 +0300

    "Added Schedule and ScheduleEntry"

---
+++ app/src/test/java/gr/aueb/softeng/project1801/DomainModelTests/TestScheduleEntry.java
@@ -0,0 +1,76 @@
+package gr.aueb.softeng.project1801.DomainModelTests;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import gr.aueb.softeng.project1801.ScheduleEntry;
+import gr.aueb.softeng.project1801.SystemCalendar;
+
+/**
+ * Created by George Chatzopoulos on 04/25/2018.
+ */
+
+public class TestScheduleEntry {
+
+    SystemCalendar calendar;
+    String DepartureTime;
+    int DayOfWeek;
+
+    @Before
+    public void SetUp(){
+        calendar = new SystemCalendar(2018,4,25);
+        DepartureTime = "9:00AM";
+        DayOfWeek = calendar.getDayOfMonth();
+    }
+
+
+    @Test
+    public void createScheduleEntry(){
+        ScheduleEntry entry = new ScheduleEntry();
+        entry.setCalendar(calendar);
+        entry.setDepartureTime(DepartureTime);
+    }
+
+    @Test
+    public void createFormSceduleEntry(){
+        ScheduleEntry entry = new ScheduleEntry("9;00AM", new SystemCalendar(2018,4,25));
+    }
+
+    @Test
+    public void getterDayOfWeek(){
+        ScheduleEntry entry = new ScheduleEntry();
+        entry.setCalendar(calendar);
+        entry.setDepartureTime(DepartureTime);
+        Assert.assertEquals(25,entry.getDayOfWeek());
+    }
+
+    @Test
+    public void getterDepartureTime(){
+        ScheduleEntry entry = new ScheduleEntry();
+        entry.setCalendar(calendar);
+        entry.setDepartureTime(DepartureTime);
+        Assert.assertEquals("9:00AM",entry.getDepartureTime());
+    }
+
+    @Test
+    public void getterCalendar(){
+        ScheduleEntry entry = new ScheduleEntry();
+        entry.setCalendar(calendar);
+        entry.setDepartureTime(DepartureTime);
+        Assert.assertEquals(2018,calendar.getYear());
+        Assert.assertEquals(4,calendar.getMonth());
+        Assert.assertEquals(25,calendar.getDayOfMonth());
+    }
+
+
+
+
+
+
+
+
+
+
+}
