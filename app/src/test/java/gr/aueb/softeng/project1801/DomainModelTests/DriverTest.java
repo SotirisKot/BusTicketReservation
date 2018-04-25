b8905203f654b0b02467c725b4c48a107f22f037
tasos <tasoslepiaps@gmail.com>
2018-04-25 19:00:30 +0300

    Initial

--- app/src/test/java/gr/aueb/softeng/project1801/DomainModelTests/DriverTest.java
+++ app/src/test/java/gr/aueb/softeng/project1801/DomainModelTests/DriverTest.java
@@ -2,8 +2,12 @@ package gr.aueb.softeng.project1801.DomainModelTests;
 
 import org.junit.Assert;
 import org.junit.Test;
+
+import gr.aueb.softeng.project1801.BusState;
 import gr.aueb.softeng.project1801.Driver;
 import gr.aueb.softeng.project1801.DriverState;
+
+import static org.junit.Assert.assertTrue;
 
 
 public class DriverTest {
@@ -24,5 +28,63 @@ public class DriverTest {
         driver.available();
         Assert.assertEquals(DriverState.AVAILABLE,driver.getState());
     }
+
+    @Test
+    public void getDriverName() {
+        Driver driver = new Driver();
+        driver.setDriverName("Toni");
+        assertTrue(driver.getDriverName().equals("Toni"));
+    }
+
+    @Test
+    public void setDriverName() {
+        Driver driver = new Driver();
+        driver.setDriverName("Toni");
+        assertTrue(driver.getDriverName().equals("Toni"));
+    }
+
+    @Test
+    public void getDriverID() {
+        Driver driver = new Driver();
+        driver.setDriverID("666");
+        assertTrue(driver.getDriverID().equals("666"));
+    }
+
+    @Test
+    public void setDriverID() {
+        Driver driver = new Driver();
+        driver.setDriverID("666");
+        assertTrue(driver.getDriverID().equals("666"));
+    }
+
+    @Test
+    public void getState() {
+        Driver driver = new Driver();
+        driver.setState(DriverState.AVAILABLE);
+        assertTrue(driver.getState() == DriverState.AVAILABLE);
+    }
+
+    @Test
+    public void setState() {
+        Driver driver = new Driver();
+        driver.setState(DriverState.AVAILABLE);
+        assertTrue(driver.getState() == DriverState.AVAILABLE);
+    }
+
+    @Test
+    public void equals() {
+        String driverName = "Toni";
+        String driverID = "666";
+        Driver driver1 = new Driver();
+        Driver driver2 = new Driver();
+        driver1.setDriverName(driverName);
+        driver1.setDriverID(driverID);
+        driver2.setDriverName(driverName);
+        driver2.setDriverID(driverID);
+        assertTrue(driver1.equals(driver2));
+
+    }
+
+
 
 }
